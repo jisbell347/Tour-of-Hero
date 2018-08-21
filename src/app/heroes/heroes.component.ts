ab54b3d504d5e8f3ac09c5fda0c23a705cafd4ab
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-21 13:43:15 -0600

    added selectedHero element and event listener

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 
 @Component({
@@ -7,10 +8,18 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-   hero = HEROES;
+   heroes = HEROES;
+
+   selectedHero: Hero;
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+   onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+   }
 
 }
